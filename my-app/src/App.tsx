0642b0c50e24a738985f9dacf18d1e37fe232e0c
MaryamMohamedYahya <ummabdullahyahya@gmail.com>
2024-05-06 18:48:02 +0530

    Input box

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
 import './App.css';
+import InputFeild from './components/InputFeild';
+
 
 
 
 const App: React.FC = () =>{
 
 return  <div className="App">
-  <span className='heading'>Todo</span>
+  <span className="heading">Let's do</span>
+
+<InputFeild />
+
 </div>
 
 };
---
+++ my-app/src/components/InputFeild.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InputFeild = () =>{
+    return( <form className="input">
+    <input type="input" placeholder='What shall we do?'  className='input_box'/>
+
+</form>
+)
+       
+};
+export default InputFeild ;
