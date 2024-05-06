30ccde5a0fe83d5e9e6bbf358436513d0b2e2ecf
MaryamMohamedYahya <ummabdullahyahya@gmail.com>
2024-05-06 23:33:45 +0530

    Todo

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 import './App.css';
 import Input from './components/Input';
+import {Todo} from"./model";
+import TodoList from './components/Todolist';
 
 const App: React.FC = () =>{
-  const[todo, setTodo] = useState<string>("");
 
-return  <div className="App">
+  const[todo, setTodo] = useState<string>("");
+  const[todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo){
+      setTodos([...todos, {id:Date.now(), todo, isDone:false}]);
+      setTodo("");
+    }
+  };
+    
+  
+
+  console.log(todo);
+
+return  (<div className="App">
   <span className="heading">TODO</span>
 
-<Input  todo={todo} setTodo={setTodo} />
+<Input  todo={todo} setTodo={setTodo}  handleAdd={handleAdd}/>
+ <TodoList />
+
+
 
 </div>
 
+);
 };
 export default App;
---
+++ my-app/src/components/Todolist.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Todo} from "../model";
+import "./styles.css"
+const TodoList:React.FC = ({ todos, setTodos}) =>{
+    return <div className='todos'>{
+    
+}
+    </div>;
+};
+export default TodoList;
