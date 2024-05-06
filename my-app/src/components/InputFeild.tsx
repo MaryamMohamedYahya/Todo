0e342383ac241292a0af94f4aea7f83723c3f625
MaryamMohamedYahya <ummabdullahyahya@gmail.com>
2024-05-06 19:09:45 +0530

    Box

--- my-app/src/components/InputFeild.tsx
+++ my-app/src/components/InputFeild.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import "./styles.css";
 
 const InputFeild = () =>{
     return( <form className="input">
     <input type="input" placeholder='What shall we do?'  className='input_box'/>
+    <button  className="input_submit" type='submit'>
+        Do
+    </button>
 
 </form>
 )
