ee467400fa47089afbc24f8d18c2e3625fd52f44
MaryamMohamedYahya <ummabdullahyahya@gmail.com>
2024-05-06 17:41:13 +0530

    Add

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+
+
+const App: React.FC = () =>{
+
+return  <div className="App">
+  <span className='heading'>Todo</span>
+</div>
+
+};
+export default App;
